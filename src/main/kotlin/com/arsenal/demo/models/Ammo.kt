ab9eb5df64648539a4afd8f161e96f0921e4b161
Valyon1886 <64031088+Valyon1886@users.noreply.github.com>
2023-02-15 17:46:26 +0300

    are you know kotlin?

---
+++ src/main/kotlin/com/arsenal/demo/models/Ammo.kt
@@ -0,0 +1,15 @@
+package com.arsenal.demo.models
+
+
+class Ammo (name: String, amount: Int){
+
+    val name: String
+
+    val amount: Int
+
+    init{
+        this.name=name
+        this.amount=amount
+    }
+
+}
