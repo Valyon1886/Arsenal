64b9994a9ae9aeaf21ba7881c4c8cb6899250026
Valyon1886 <64031088+Valyon1886@users.noreply.github.com>
2023-03-05 19:20:57 +0300

    repositories

---
+++ src/main/kotlin/com/arsenal/demo/repos/Repositories.kt
@@ -0,0 +1,17 @@
+package com.arsenal.demo.repos
+
+import com.arsenal.demo.models.Ammo
+import com.arsenal.demo.models.Blaster
+import com.arsenal.demo.models.User
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UserRepository : JpaRepository<User, Long>{
+
+}
+
+@Repository
+interface BlasterRepository : JpaRepository<Blaster, Long>{
+
+}
