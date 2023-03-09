e4a14953ec1e269c7c5bd9c9f5021e4e4308128f
Valyon1886 <64031088+Valyon1886@users.noreply.github.com>
2023-03-09 20:38:05 +0300

    editing blasterController

--- src/main/kotlin/com/arsenal/demo/controllers/BlasterController.kt
+++ src/main/kotlin/com/arsenal/demo/controllers/BlasterController.kt
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
 
 @Controller
-@RequestMapping("/blasters")
+@RequestMapping("/blaster")
 class BlasterController(private val blasterService: BlasterService) {
 
     @PostMapping("/new")
