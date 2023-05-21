288fb620cc59ba1f1dde2c6871c999f632b6448e
Valyon1886 <samonchev001@gmail.com>
2023-05-22 01:55:23 +0300

    reFUCKTORING (destroying enums)

--- src/main/kotlin/com/arsenal/demo/models/Game.kt
+++ src/main/kotlin/com/arsenal/demo/models/Game.kt
@@ -7,13 +7,15 @@ import org.hibernate.internal.util.collections.CollectionHelper
 @Table(name="game")  //  Таблица игровых сеансов
 data class Game  (
     @Column(nullable = false) var name: String,  // Название игрового сеанса
-    @Column(nullable = true)  var amount: Int,  // Минимальное количество участников
-    @Column(nullable = true) var image: String,  // Иконка игрового сеанса
-    @Column(nullable = false) var mode: Mode,  // Игровой режим (правила игрового сеанса)
+    @Column(nullable = true)  var amount: Int?,  // Минимальное количество участников
+    @Column(nullable = true) var image: String?,  // Иконка игрового сеанса
+    @Column(nullable = false) var mode: String,  // Игровой режим (правила игрового сеанса)
+    @Column(nullable = true) var description: String?,
+    @Column(nullable = true) var destination: String?,
 //    @Column(nullable = true) var arsenals: MutableList<MutableList<Blaster>>?,  // Список
     @Column(nullable = true) var users: MutableList<Long>?,  // Список
     @Id @GeneratedValue var id: Long? = null
 )
 {
-    constructor() : this("",  1, "", Mode.CLASSIC_THIRD, mutableListOf())
+    constructor() : this("",  1, "", "CLASSIC_THIRD", "", "", mutableListOf())
 }
