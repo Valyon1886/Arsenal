5f3d2d86b362f20508c85ba22ace32d07da422df
Valyon1886 <samonchev001@gmail.com>
2023-05-21 22:52:54 +0300

    reFUCKTORING

--- src/main/kotlin/com/arsenal/demo/models/User.kt
+++ src/main/kotlin/com/arsenal/demo/models/User.kt
@@ -10,15 +10,15 @@ import org.hibernate.internal.util.collections.CollectionHelper.listOf
 data class User(
     @Column var idToken: String?,
     @Column(nullable = false) var userName: String,  // Никнейм пользователя
-    @Column(nullable = false) val password: String,  // Пароль пользователя (хранится в шифрованном виде)
+//    @Column(nullable = false) val password: String,  // Пароль пользователя (хранится в шифрованном виде)
     @Column(nullable = true) var image: String?,  // Аватар пользователя
-    @Column(nullable = true) var role: Role,  // Роль пользователя (user или admin)
+    @Column(nullable = true) var role: String,  // Роль пользователя (user или admin)
     @OneToMany @Column(nullable = true) var arsenal: MutableList<Blaster>?,  // Список оборудования, добавленных пользователем в арсенал
     @OneToMany @Column(nullable = true) var games: MutableList<Game>?,  // Список игровых сессий, в которых учавствовал пользователь
     @Id @GeneratedValue var id: Long? = null
 )
 {
 
-    constructor() : this("","", "", "", Role.UNAUTHORIZED, mutableListOf(), mutableListOf())
+    constructor() : this("","",  "", "user", mutableListOf(), mutableListOf())
 }
 
